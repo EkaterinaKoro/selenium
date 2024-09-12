3bb26480353b204c8e2ba4982cfcee654a8123c3
Kate <cimonyu@mail.ru>
2024-09-12 17:50:12 +0300

    Исправлена ошибка с драйвером

--- src/test/java/ru/netology/applicationСardTest.java
+++ src/test/java/ru/netology/applicationСardTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +17,7 @@ public class applicationСardTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
